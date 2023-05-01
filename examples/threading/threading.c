0f10431ff4f22019ca88f8bbc003a6315474e492
AbanoubSaid1 <abanoubsaid2@gmail.com>
2023-05-01 14:27:28 -0400

    Thread Implemented

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
+
 
 // Optional: use these functions to add debug or error prints to your application
 #define DEBUG_LOG(msg,...)
@@ -11,23 +13,37 @@
 void* threadfunc(void* thread_param)
 {
 
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
-    // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
-    return thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+     usleep(thread_func_args->wait_to_obtain_ms*1000);
+    int res = pthread_mutex_lock(thread_func_args->mutex);
+    if(res != 0){
+		thread_func_args->thread_complete_success = false;
+	}else {
+		usleep(thread_func_args->wait_to_release_ms*1000);
+		res = pthread_mutex_unlock(thread_func_args->mutex);
+		if(res != 0){
+			thread_func_args->thread_complete_success = false;
+		}
+		thread_func_args->thread_complete_success = true;
+	}
+    pthread_exit(thread_param);
 }
 
 
 bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
 {
-    /**
-     * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
-     * using threadfunc() as entry point.
-     *
-     * return true if successful.
-     *
-     * See implementation details in threading.h file comment block
-     */
-    return false;
+    struct thread_data *thread_param = (struct thread_data *) malloc(sizeof(struct thread_data));
+    thread_param->mutex = mutex;
+	thread_param->wait_to_obtain_ms = wait_to_obtain_ms; 
+	thread_param->wait_to_release_ms = wait_to_release_ms;
+
+    int res = pthread_create(thread, NULL, threadfunc, (void*) thread_param);
+    if(res != 0){
+		printf("pthread created failed: %d\n", res);
+		return false;
+	}
+    else{
+    return true;
+    }
 }
 
